e1b37858f29360ca62da6c2b06c1718f7af889fd
italosnunes <46608041+italosnunes@users.noreply.github.com>
2020-07-17 19:19:08 -0300

    authContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalStyle />
-    <SignIn />
   </>
 );
 
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,10 +1,9 @@
-import React, { useRef, useCallback } from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import * as Yup from 'yup';
-import {Form} from '@unform/web';
+import React, { useCallback, useRef} from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
-import getValidationErrors from '../../utils/getValidationErrors';
-import {useAuth} from '../../hooks/AuthContext';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import logo from '../../assets/logo.svg';
 
@@ -13,32 +12,21 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-interface SignInFormData {
-  email: string;
-  password:string;
-}
-
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
 
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useAuth();
-
-  const handleSubmit = useCallback(async(data:SignInFormData) => {
+  const handleSubmit = useCallback(async(data:object) => {
     try {
       formRef.current?.setErrors({});
       const schema = Yup.object().shape({
+        name:Yup.string().required('Nome obrigatório'),
         email:Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
-        password:Yup.string().required('Senha obrigatória'),
+        password:Yup.string().min(6,'No mínimo 6 dígitos'),
       })
 
       await schema.validate(data, {
         abortEarly:false,
-      });
-
-      signIn({
-        email: data.email,
-        password:data.password,
       });
 
     } catch (err) {
@@ -46,31 +34,33 @@ const SignIn: React.FC = () => {
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }
-  },[signIn])
+  },[])
 
- return(
+  return(
     <Container>
+      <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
         <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu login</h1>
+          <h1>Faça seu cadastro</h1>
 
+          <Input name="name" type="text" icon={FiUser} placeholder="Nome" />
           <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
           <Input name="password" type="password" icon={FiLock} placeholder="Password" />
 
-          <Button type="submit">Entrar</Button>
+          <Button type="submit">Cadastrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
         </Form>
 
         <a href="login">
-          <FiLogIn />
-          Criar Conta
+          <FiArrowLeft />
+          Voltar para logon
         </a>
       </Content>
-      <Background />
+    
     </Container>
   )
 };
 
-export default SignIn;
+export default SignUp;
